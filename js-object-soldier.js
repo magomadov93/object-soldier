dc220743c4c22777ae4267fde797edaf3867a3f8
magomadov93 <madoldelax@mail.ru>
2022-06-10 13:16:29 +0300

    сделал первый sabtask

---
+++ js-object-soldier.js
@@ -0,0 +1,23 @@
+const soldier = {
+    nameSoldier: "Adam",
+    health: 10,
+    weapon: {
+        nameweapon: 'AK-47',
+        cartridges: 30   
+        },
+    supplies:3,
+
+    }
+    
+
+fire: function(){
+
+},
+
+recharge: function(){
+
+},
+
+hurt: function(){
+
+},
