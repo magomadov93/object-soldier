e256512bfe063df11376f4a44c3516b150610de4
magomadov93 <madoldelax@mail.ru>
2022-06-11 17:16:32 +0300

    сделал второй sabtask

--- js-object-soldier.js
+++ js-object-soldier.js
@@ -3,21 +3,35 @@ const soldier = {
     health: 10,
     weapon: {
         nameweapon: 'AK-47',
-        cartridges: 30   
+        cartridges: 30 
         },
     supplies:3,
 
-    }
     
-
-fire: function(){
-
-},
-
-recharge: function(){
-
-},
-
-hurt: function(){
-
-},
+     fire: function(){
+       this.weapon.cartridges--
+         console.log("бах-бах")
+       if(this.weapon.cartridges === 0){
+         return "обойма пуста.Перезарядить"
+       }
+      },
+       
+     recharge: function(){
+       this.weapon.cartridges = 30
+       this.supplies--
+       console.log("перезарядка")
+       if(this.supplies === 0){
+       return "не осталось припасов"
+      }
+     },
+     hurt: function(){
+       this.health--
+       if(this.health === 0){
+        console.log("ты проиграл")
+       }
+     }
+   };
+soldier.fire()
+soldier.recharge()
+soldier.hurt()
+console.log(soldier)
