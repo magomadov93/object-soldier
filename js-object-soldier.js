d1c465e41bbd3a9aca4abd5e0f2eceeec3686f97
magomadov93 <madoldelax@mail.ru>
2022-06-12 16:20:36 +0300

    сделал второй sabtask

--- js-object-soldier.js
+++ js-object-soldier.js
@@ -1,37 +1,31 @@
 const soldier = {
-    nameSoldier: "Adam",
-    health: 10,
-    weapon: {
-        nameweapon: 'AK-47',
-        cartridges: 30 
-        },
-    supplies:3,
+  nameSoldier: "Adam",
+  health: 3,
+  weapon: {
+    nameweapon: "AK-47",
+    cartridges: 3,
+  },
+  supplies: 3,
 
-    
-     fire: function(){
-       this.weapon.cartridges--
-         console.log("бах-бах")
-       if(this.weapon.cartridges === 0){
-         return "обойма пуста.Перезарядить"
-       }
-      },
-       
-     recharge: function(){
-       this.weapon.cartridges = 30
-       this.supplies--
-       console.log("перезарядка")
-       if(this.supplies === 0){
-       return "не осталось припасов"
-      }
-     },
-     hurt: function(){
-       this.health--
-       if(this.health === 0){
-        console.log("ты проиграл")
-       }
-     }
-   };
-soldier.fire()
-soldier.recharge()
-soldier.hurt()
-console.log(soldier)
+  fire: function () {
+    if (this.weapon.cartridges === 0) {
+      return "обойма пуста.Перезарядитесь";
+    }
+    this.weapon.cartridges--;
+    return "бах-бах";
+  },
+  recharge: function () {
+    if (this.supplies === 0) {
+      return "не осталось припасов";
+    }
+    this.weapon.cartridges = 30;
+    this.supplies--;
+    return "перезарядка";
+  },
+  hurt: function () {
+    if (this.health === 0) {
+      return "ты проиграл";
+    }
+    return this.health--;
+  },
+};
